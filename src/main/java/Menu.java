0d7a4dfe340fcaf5eed2afbe2b36555b96fa3f4d
yasnakateb <yasna.katebzadeh@gmail.com>
2020-12-15 17:27:19 +0330

    Update Menu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,7 +1,21 @@
+import java.util.Scanner; 
+
 public class Menu
 { 
     public static void main(String args[]) 
     { 
-        System.out.println("Menu"); 
+        System.out.println("Menu:"); 
+        System.out.println("1.Salespaerson"); 
+        System.out.println("2.Customer"); 
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        if (i == 1){
+            System.out.println("Hello dude!"); 
+        }    
+        else{
+            System.out.println("Welcome to our supermarket!"); 
+        }
+
     } 
 } 
