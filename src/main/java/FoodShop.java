04ced10d76309e3682b9c06d4c17d314f5087223
yasnakateb <yasna.katebzadeh@gmail.com>
2021-01-01 13:42:14 +0330

    Add FoodShop

---
+++ src/main/java/FoodShop.java
@@ -0,0 +1,13 @@
+import java.util.*; 
+
+public class FoodShop{
+    int city_id;
+    String name;
+    int min_bill_val;
+    String about;
+    String address_text;
+    float geo_x;
+    float geo_y;
+    
+    
+} 
