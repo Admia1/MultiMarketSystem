ac0ef88f52e72701282d7f232a48e19eb9e8c1f9
yasnakateb <yasna.katebzadeh@gmail.com>
2021-01-01 13:05:52 +0330

    FoodShopCity: Add methods and constructor

--- src/main/java/FoodShopCity.java
+++ src/main/java/FoodShopCity.java
@@ -6,4 +6,20 @@ public class FoodShopCity
     float geo_x;
     float geo_y;
 
+    public FoodShopCity(String name, float geo_x, float geo_y){
+        this.name = name;
+        this.geo_x = geo_x;
+        this.geo_y = geo_y;
+
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+
+    void setPosition(float geo_x, float geo_y){
+        this.geo_x = geo_x;
+        this.geo_y = geo_y;
+    }
+
 } 
