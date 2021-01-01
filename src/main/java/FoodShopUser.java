132f0cdd7494dd3884d504791fd82bf8261577da
yasnakateb <yasna.katebzadeh@gmail.com>
2021-01-01 13:25:08 +0330

    Add FoodShopUser

---
+++ src/main/java/FoodShopUser.java
@@ -0,0 +1,20 @@
+import java.util.*; 
+
+public class FoodShopUser
+{ 
+    String first_name;
+    String last_name;
+    String password;
+    String email;
+    String phone_number;
+
+    public FoodShopUser(String first_name, String last_name, String password, String email, String phone_number){
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.password = password;
+        this.email = email;
+        this.phone_number = phone_number;
+
+    }
+
+} 
