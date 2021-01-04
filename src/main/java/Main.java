cb46afedad8ef92d7bb92ace664f8191414c1386
yasnakateb <yasna.katebzadeh@gmail.com>
2021-01-04 07:40:05 +0330

    Test database connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,33 @@
-import java.util.Scanner; 
+
+import java.util.Scanner;
 import java.util.*;
+import java.sql.*;
+
 
 public class Main
-{ 
-    public static void main(String args[]) 
-    { 
-        System.out.println("This is a test."); 
-        
-    } 
-} 
+{
+    public static void main(String args[])
+    {
+        String url = "jdbc:mysql://localhost:3306/online_grocerystore_management_system";
+		String username = "root";
+		String password = "rootpass";
+
+		System.out.println("Loading driver...");
+
+		try {
+		    Class.forName("com.mysql.cj.jdbc.Driver");
+		    System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+		    throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+		}
+
+		System.out.println("Connecting database...");
+
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+	    	System.out.println("Database connected!");
+			} catch (SQLException e) {
+	    throw new IllegalStateException("Cannot connect the database!", e);
+		}
+
+	}
+}
