62d02ea5897d175d201d7e56da7a8cbb5a150d8f
yasnakateb <yasnakatebzadeh@gmail.com>
2021-01-08 18:18:36 +0330

    Add class Store

---
+++ src/main/java/Store.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Store{
+    String store_name;
+    String store_type;
+    String store_description;
+
+
+    public void setName(String name){
+        this.store_name = name;
+    }
+
+    public String getName(){
+        return this.store_name;
+    }
+
+
+    public void setType(String type){
+        this.store_name = type;
+    }
+
+    public String getType(){
+        return this.store_type;
+    }
+
+
+    public void setDescription(String description){
+        this.store_description = description;
+    }
+
+    public String getDescription(){
+        return this.store_description;
+    }
+
+    public boolean addStores(){
+        return true;
+    }
+
+    public boolean deleteStores(){
+        return true;
+    }
+
+    public boolean updateStores(){
+        return true;
+    }
+
+    public boolean searchStores(){
+        return true;
+    }
+}    
