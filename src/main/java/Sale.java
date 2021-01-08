0bbfe8b0d1d638928fcf4d1154236830def1a212
yasnakateb <yasnakatebzadeh@gmail.com>
2021-01-08 20:31:23 +0330

    Add class Sale

---
+++ src/main/java/Sale.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.math.BigDecimal;
+
+public class Sale{
+
+    String sales_type;
+    String sale_description;;
+    String sale_amount;
+
+    public Sale(String type, String description, String amount){
+        this.sale_type = type;
+        this.sale_description = description;
+        this.sale_amount = amount;
+        
+    }
+
+    public void setDate(String date){
+        this.sale_date = date;
+    }
+
+    public void setDescription(String description){
+        this.sale_description = description;
+    }
+
+    public void setAmount(String amount){
+        this.sale_amount = amount;
+    }
+
+    public String getDate(){
+        return this.sale_date;
+    }
+
+    public String getDescription(){
+        return this.sale_description;
+    }
+
+    public String getAmount(){
+        return this.sale_amount;
+    }
+
+
+    public boolean addSales(){
+        return true;
+    }
+
+    public boolean deleteSales(){
+        return true;
+    }
+
+    public boolean updateSales(){
+        return true;
+    }
+
+    public boolean searchSales(){
+        return true;
+    }
+
+    public boolean saveSales(){
+        return true;
+    }
+
+}
+
