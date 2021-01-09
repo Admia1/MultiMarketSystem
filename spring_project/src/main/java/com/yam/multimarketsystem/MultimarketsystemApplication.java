8af647bde8b5bfee6518a16b70b383e4039b22a3
admia1 <sampadpars@gmail.com>
2021-01-09 06:23:30 +0330

    simple spring code as base form https://spring.io/guides/gs/accessing-data-mysql/ state : working

---
+++ spring_project/src/main/java/com/yam/multimarketsystem/MultimarketsystemApplication.java
@@ -0,0 +1,13 @@
+package com.yam.multimarketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimarketsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimarketsystemApplication.class, args);
+	}
+
+}
